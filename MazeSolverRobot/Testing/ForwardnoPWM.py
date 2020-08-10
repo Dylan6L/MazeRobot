9cea7a659075f1c645f1917ed2ec51b9b7b59a9b
Dylan6Larson <dylan6larson@gmail.com>
2020-08-10 07:57:38 -0700

    Created a better encoder test inside ForwardnoPWM.py

--- MazeSolverRobot/Testing/ForwardnoPWM.py
+++ MazeSolverRobot/Testing/ForwardnoPWM.py
@@ -5,9 +5,40 @@ import time
 prPWM = 18
 plPWM = 19
 
+# Left Front Encoder Value
+leftFE = 0
+leftBE = 0
+rightFE = 0
+rightBE = 0
+
+def sensorCallback(channel):
+    global cLeftFE, cLeftBE, cRightFE, cRightBE, leftFE, leftBE, rightFE, rightBE
+    # If no input then sensor went high, add value to value variable
+    if not GPIO.input(channel):
+        if channel == cLeftFE:
+            leftFE += 1
+        elif channel == cLeftBE:
+            leftBE += 1
+        elif channel == cRightFE:
+            rightFE += 1
+        elif channel == cRightBE:
+            rightBE += 1
+
 try:
 
     GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(cLeftFE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(cLeftFE, GPIO.BOTH, callback=sensorCallback, bouncetime=20)
+    GPIO.setup(cLeftBE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(cLeftBE, GPIO.BOTH, callback=sensorCallback, bouncetime=20)
+    GPIO.setup(cRightFE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(cRightFE, GPIO.BOTH, callback=sensorCallback, bouncetime=20)
+    GPIO.setup(cRightBE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(cRightBE, GPIO.BOTH, callback=sensorCallback, bouncetime=20)
+
+    GPIO.setup(plPWM, GPIO.OUT)
+    GPIO.setup(prPWM, GPIO.OUT)
 
     lp = GPIO.PWM(plPWM, 100)
     lp.start(0)
@@ -29,7 +60,9 @@ try:
     GPIO.output(13, False)
     GPIO.output(6, True)
 
-    time.sleep(5)
+    for i in range(0, 100):
+        time.sleep(0.5)
+        print(f"leftFE: {leftFE} leftBE: {leftBE} rightFE: {rightFE} rightBE: {rightBE}")
 
     GPIO.output(5, False)
     GPIO.output(19, False)
